428ef210feade4972aef0b5f4a847557c3167cb6
luc12001 <luc12001@byui.edu>
2022-02-02 09:13:19 -0600

    Prepared notes linking through main page

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,8 @@
 const links = [{ label: "Week 1 notes", url: "/WDD330/Week1/index.html"}, 
                 { label: "Week 2 notes", url: "/WDD330/Week2/index.html"},
                 { label: "Week 3 notes", url: "/WDD330/Week3/index.html"},
-                { label: "Week 4 notes", url: "/WDD330/Week4/index.html"}];
+                { label: "Week 4 notes", url: "/WDD330/Week4/index.html"},
+                { label: "Week 5 notes", url: "/WDD330/Week5/index.html"}];
 
                 let list = document.getElementById("links");
 
