f847dfd736a596ebdb94e1ae81e194054b60fe56
luc12001 <luc12001@byui.edu>
2022-02-11 13:46:43 -0600

    updated index added week 6 content

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,8 @@ const links = [{ label: "Week 1 notes", url: "/WDD330/Week1/index.html"},
                 { label: "Week 2 notes", url: "/WDD330/Week2/index.html"},
                 { label: "Week 3 notes", url: "/WDD330/Week3/index.html"},
                 { label: "Week 4 notes", url: "/WDD330/Week4/index.html"},
-                { label: "Week 5 notes", url: "/WDD330/Week5/index.html"}];
+                { label: "Week 5 notes", url: "/WDD330/Week5/index.html"},
+                { label: "Week 6 notes", url: "/WDD330/Week6/index.html"}];
 
                 let list = document.getElementById("links");
 
