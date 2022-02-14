6f381db4d302705d1fea87f1ef31999c946606dd
luc12001 <luc12001@byui.edu>
2022-02-14 10:26:50 -0600

    Added Week 7 notes to main navigation

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,8 @@ const links = [{ label: "Week 1 notes", url: "/WDD330/Week1/index.html"},
                 { label: "Week 3 notes", url: "/WDD330/Week3/index.html"},
                 { label: "Week 4 notes", url: "/WDD330/Week4/index.html"},
                 { label: "Week 5 notes", url: "/WDD330/Week5/index.html"},
-                { label: "Week 6 notes", url: "/WDD330/Week6/index.html"}];
+                { label: "Week 6 notes", url: "/WDD330/Week6/index.html"},
+                { label: "Week 7 notes", url: "/WDD330/Week7/index.html"}];
 
                 let list = document.getElementById("links");
 
