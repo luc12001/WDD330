8219eb65f7f5b0fe8d1df43b71263e8f3e7b798a
luc12001 <luc12001@byui.edu>
2022-03-18 11:17:57 -0500

    links are hard sometimes

--- Week10/Quake/js/index.js
+++ Week10/Quake/js/index.js
@@ -1,5 +1,5 @@
-import QuakesController from '/WDD330/Week10/Quake/QuakesController.js';
-import buildNavigation from '/WDD330/Week10/Quake/routing.js';
+import QuakesController from '../QuakesController.js';
+import buildNavigation from '../routing.js';
 
 const navElement = document.getElementById('mainNav');
 buildNavigation(navElement);
